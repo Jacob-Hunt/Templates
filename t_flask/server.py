c521f30d47ae48db36019bef8a734f0204560c13
Jacob Hunt <jacobhuntemail@gmail.com>
2017-04-12 23:58:19 -0400

    Cleaned up formatting

--- t_flask/server.py
+++ t_flask/server.py
@@ -19,15 +19,21 @@ def form():
 # html form demo result page
 @app.route("/result", methods = ["GET", "POST"])
 def result():
+    
     if request.method == "POST":
         r_first = request.form.get("first")
         r_middle = request.form.get("middle")
         r_last = request.form.get("last")
+    
     if request.method == "GET":
         r_first = request.args.get("first")
         r_middle = request.args.get("middle")
         r_last = request.args.get("last")
-    return render_template("result.html", first=r_first, middle=r_middle, last=r_last)
+    
+    return render_template("result.html",
+                           first = r_first,
+                           middle = r_middle,
+                           last = r_last)
 
 # configure server
 server_address = "0.0.0.0"
